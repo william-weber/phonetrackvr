8ec545a8c2117a9653d266b9fd3d0d3c951ee6d8
William Weber <taj.tek@gmail.com>
2026-04-11 19:45:32 -0400

    display image in overlay

--- Assets/Scripts/Overlay.cs
+++ Assets/Scripts/Overlay.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 using System;
+using System.Runtime.InteropServices;
+using System.Numerics;
 
 public class Overlay : MonoBehaviour
 {
@@ -13,26 +15,31 @@ public class Overlay : MonoBehaviour
 
         InitOpenVR();
 
-        var key = "PhoneTrackVR.Overlay";
-        var name = "PhoneTrackVR Overlay";
-        var error = OpenVR.Overlay.CreateOverlay(key, name, ref overlayHandle);
-        if (error != EVROverlayError.None)
-        {
-            throw new Exception("Failed to create overlay: " + error.ToString());
+        overlayHandle = CreateOverlay("PhoneTrackVROverlayKey", "PhoneTrackVR");
+
+        // draw sample image
+        var filePath = Application.streamingAssetsPath + "/vrchat.png";
+        var error = OpenVR.Overlay.SetOverlayFromFile(overlayHandle, filePath);
+        if (error != EVROverlayError.None)        {
+            throw new Exception("Failed to set overlay image: " + error);
         }
+
+
+        //SetOverlayFromFile(overlayHandle, filePath);
+
+        //SetOverlaySize(overlayHandle, 0.5f);
+
+        //var position = new  UnityEngine.Vector3(0, 2, 3);
+        //var rotation = UnityEngine.Quaternion.Euler(0, 0, 45);
+        //SetOverlayTransformAbsolute(overlayHandle, position, rotation);
+
+        ShowOverlay(overlayHandle);
 
     }
 
     private void OnApplicationQuit()
     {
-        if (overlayHandle != OpenVR.k_ulOverlayHandleInvalid)
-        {
-            var error = OpenVR.Overlay.DestroyOverlay(overlayHandle);
-            if (error != EVROverlayError.None)            {
-                Debug.LogError("Failed to destroy overlay: " + error.ToString());
-                throw new Exception("Failed to destroy overlay: " + error.ToString());
-            }
-        }
+        DestroyOverlay(overlayHandle);
     }
 
     // Update is called once per frame
@@ -57,4 +64,65 @@ public class Overlay : MonoBehaviour
             throw new Exception("Failed to initialize OpenVR: " + error.ToString());
         }
     }
+
+    private ulong CreateOverlay(string key, string name)
+    {
+        var handle = OpenVR.k_ulOverlayHandleInvalid;
+        var error = OpenVR.Overlay.CreateOverlay(key, name, ref handle);
+        if (error != EVROverlayError.None)
+        {
+            throw new Exception("Failed to create overlay: " + error.ToString());
+        }
+        return handle;
+    }
+
+    private void DestroyOverlay(ulong handle)
+    {
+        if (handle != OpenVR.k_ulOverlayHandleInvalid)
+        {
+            var error = OpenVR.Overlay.DestroyOverlay(handle);
+            if (error != EVROverlayError.None)
+            {
+                throw new Exception("Failed to destroy overlay: " + error.ToString());
+            }
+        }
+    }
+
+    private void SetOverlayFromFile(ulong handle, string path)
+    {
+        var error = OpenVR.Overlay.SetOverlayFromFile(handle, path);
+        if (error != EVROverlayError.None)
+        {
+            throw new Exception("Failed to set overlay image: " + error.ToString());
+        }
+    }
+
+    private void ShowOverlay(ulong handle)
+    {
+        var error = OpenVR.Overlay.ShowOverlay(handle);
+        if (error != EVROverlayError.None)
+        {
+            throw new Exception("Failed to show overlay: " + error.ToString());
+        }
+    }
+
+    private void SetOverlaySize(ulong handle, float size)
+    {
+        var error = OpenVR.Overlay.SetOverlayWidthInMeters(handle, size);
+        if (error != EVROverlayError.None)
+        {
+            throw new Exception("Failed to set overlay width: " + error.ToString());
+        }
+    }
+
+    // private void SetOverlayTransformAbsolute(ulong handle, UnityEngine.Vector3 position, UnityEngine.Quaternion rotation)
+    // {
+    //     var rigidTransform = new SteamVR_Utils.RigidTransform(position, rotation);
+    //     var matrix = rigidTransform.ToHmdMatrix34();
+    //     var error = OpenVR.Overlay.SetOverlayTransformAbsolute(handle, ETrackingUniverseOrigin.TrackingUniverseStanding, ref matrix);
+    //     if (error != EVROverlayError.None)
+    //     {
+    //         throw new Exception("Failed to set overlay transform: " + error.ToString());
+    //     }
+    // }
 }
